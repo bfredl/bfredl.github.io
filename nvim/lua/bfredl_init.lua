cda959a85c5527348b2cfa749ceefae8a2aef01c
Björn Linse <bjorn.linse@gmail.com>
2020-08-27 13:58:06 +0200

    autocmd: VimEnter

--- nvim/lua/bfredl_init.lua
+++ nvim/lua/bfredl_init.lua
@@ -1,4 +1,4 @@
-_G._bfredl_loaded = true
+local first_run = not _G._bfredl_vimenter
 
 require'packer'.startup(function ()
   use 'norcalli/snippets.nvim'
@@ -18,3 +18,16 @@ end]];
     };
   }
 end
+
+
+function _bfredl_vimenter()
+  _snippets_setup()
+end
+
+if first_run then
+  vim.cmd [[autocmd VimEnter * lua _bfredl_vimenter()]]
+else
+  _bfredl_vimenter()
+end
+
+_G._bfredl_loaded = true
