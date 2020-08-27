3350973ac5d100869b6bea4b11bedd6e62053e77
Björn Linse <bjorn.linse@gmail.com>
2020-08-27 13:03:27 +0200

    snippets test

--- nvim/lua/bfredl_init.lua
+++ nvim/lua/bfredl_init.lua
@@ -3,3 +3,13 @@ _G._bfredl_loaded = true
 require'packer'.startup(function ()
     use 'norcalli/snippets.nvim'
 end)
+
+function _snippets_setup()
+  local s = require'snippets'
+  s.use_suggested_mappings()
+  s.snippets = {
+    _global = {
+      todob = "TODO(bfredl) :";
+    }
+  }
+end
