b6fe51771ed97ce872fd81a92e313ac855fe7e8c
Björn Linse <bjorn.linse@gmail.com>
2020-09-03 07:57:40 +0200

    can ino theme logic

--- nvim/lua/colorbase.lua
+++ nvim/lua/colorbase.lua
@@ -1,18 +1,32 @@
 -- borrowed from norcallis lua pile
 local h = {}
-local function h.hi(group, o)
+function h.def_hi(group, o)
   local parts = {group}
-  if o.default then table.insert(parts, "defoult")
-  if o.fg then table.insert(parts, "guifg=#"..o.fg) end
-  if o.bg then table.insert(parts, "guibg=#"..o.bg) end
+  if o.default then table.insert(parts, "defoult") end
+  if o.fg then table.insert(parts, "guifg="..o.fg) end
+  if o.bg then table.insert(parts, "guibg="..o.bg) end
   if o.ctermfg then table.insert(parts, "ctermfg="..o.ctermfg) end
   if o.ctermbg then table.insert(parts, "ctermbg="..o.ctermbg) end
   if o.attr then
     table.insert(parts, "gui="..attr)
     table.insert(parts, "cterm="..attr)
   end
-  if o.sp then table.insert(parts, "guisp=#"..sp) end
+  if o.sp then table.insert(parts, "guisp="..sp) end
 
   -- nvim.api.nvim_sett_highlig()(name, parts)
   vim.cmd ('highlight '..table.concat(parts, ' '))
+end
+
+h.colors = {
+  darkblue = "#1a2c41";
+  cyan = "#1188ee";
+}
+local c = h.colors
+
+h.basetheme = {
+	LineNr = {fg=c.cyan};
+}
+
+for k,v in pairs(h.basetheme) do
+	h.def_hi(k,v)
 end
