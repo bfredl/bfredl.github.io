8e46043db46cd47c3a71ff99d9355495a86da281
Björn Linse <bjorn.linse@gmail.com>
2020-09-20 09:59:57 +0200

    grejjor

--- nvim/lua/bfredl/git.lua
+++ nvim/lua/bfredl/git.lua
@@ -1,6 +1,10 @@
 ffi = require'ffi'
 ffi.load("/usr/lib/libgit2.so", true)
-ffi.cdef [[ void git_libgit2_init(void); ]]
-ffi.C.git_libgit2_init()
 git2 = require'git2'
 repo = git2.Repository.open('/home/bjorn/config2/')
+head = repo:head()
+head:name()
+index = repo:index()
+repo:workdir()
+repo:config()
+
