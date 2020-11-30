b212200d474612f5dc0e5efcc8c2ad592e039c7e
Björn Linse <bjorn.linse@gmail.com>
2020-11-30 22:38:11 +0100

    SUBSYSTEM

--- scratch/intersect.lua
+++ scratch/intersect.lua
@@ -2,11 +2,12 @@ local a = vim.api
 local ns = a.nvim_create_namespace "intersection"
 _G.a, _G.ns = a, ns
 
-for i = 1,30 do
+for i = 1,90 do
   a.nvim_buf_set_extmark(0, ns, 3, 0, {})
 end
 
 _G.ide = a.nvim_buf_set_extmark(0, ns, 0, 0, {end_line=6})
 print(a.nvim__buf_debug_extmarks(0, {keys=true}))
 
+--a.nvim_set_current_line(a.nvim__buf_debug_extmarks(0, {keys=true}))
 
