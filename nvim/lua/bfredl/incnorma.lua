0e1b2d30d04d29f5aaeef68e3574a7c5fe5e1d6d
bfredl <bjorn.linse@gmail.com>
2022-05-28 15:16:55 +0200

    NORMA

--- nvim/lua/bfredl/incnorma.lua
+++ nvim/lua/bfredl/incnorma.lua
@@ -1,13 +1,39 @@
+-- TODO: this should be simpler
+plug_key = vim.api.nvim_replace_termcodes("<Plug>", true, true, true)
+
+s_cursors = {}
+countt = 0
+
+function docursor()
+  countt = countt + 1
+  table.insert(s_cursors, vim.fn.getpos'.')
+  return ''
+end
+vim.keymap.set({'', '!'}, "<Plug>(incnormal-cursor)", docursor)
+
 function doit(opts)
-  vim.api.nvim_echo({{vim.inspect(opts)}}, true, {})
+  local cmd = opts.args
+  if opts.is_preview then
+    cmd = cmd .. plug_key .."(incnormal-cursor)"
+  end
+  vim.cmd {
+    cmd = "normal";
+    args = {cmd};
+    bang = opts.bang;
+    range = {opts.line1, opts.line2};
+  }
 end
 
 function previewit(opts, ns, bufnr)
-  lenny = #opts.args
-  vim.api.nvim_buf_set_extmark(0, ns, 0, 0, {end_col=lenny, hl_group="ErrorMsg"})
-  --vim.api.nvim_echo({{"ahahaha"}}, true, {})
+  opts.is_preview = true
+  s_cursors = {}
+  doit(opts)
+  for _,c in ipairs(s_cursors) do
+    vim.api.nvim_buf_add_highlight(0, ns, "IncCursor", c[2]-1, c[3]-1, c[3])
+  end
   return 1
 end
 
-vim.api.nvim_create_user_command("Norma", doit, {preview=previewit, nargs=1})
+vim.cmd [[hi IncCursor cterm=reverse gui=reverse]]
 
+vim.api.nvim_create_user_command("Norma", doit, {preview=previewit, nargs=1, bang=true, range=true})
