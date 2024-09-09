ca22cab39c903a80fda0cf7a2df6890751636cbe
bfredl <bjorn.linse@gmail.com>
2024-09-09 11:28:19 +0200

    bjëbbel

---
+++ docs/pres4/notes.md
@@ -0,0 +1,23 @@
+
+- background of unicode
+
+- The stoneage time: code pages
+  - maybe a bit about DBCS ? (optional if too much content)
+
+- some terminology:
+  - Unicode is a Standard
+  - a file is sequence of bytes, 8-bit integers
+  - in order to interprete it as a text, an **encoding** is needed
+  - ASCII
+  - IEC_8859-1, latin-1 vs cp-1252
+  - multibyte (UTF-8,dbcs) vs widechar (UTF-16/32)
+
+- fun slide: acme as first editor with UTF-8 support, ☺☻☹.c
+
+- grapheme clusters
+- emoji
+
+- multibyte support in vim and neovim:
+- first builds of vim only supported single-byte text (latin-1 and similar)
+- 'encoding' vs 'fileencoding'
+
---
+++ docs/pres4/pres.lua
@@ -0,0 +1,96 @@
+local m = dofile'/home/bfredl/config2/nvim/lua/bfredl/moonwatch.lua'
+_G.m = m
+local a = bfredl.a
+
+local sf = m.float
+_G.sf = sf
+
+local s = m.make_show("Unicode in neovim and beyond", _G.s)
+_G.s = s
+
+m.prepare()
+m.cls()
+
+clight = "#FFFFFF"
+cnormal = "#e0e0e0"
+cmiddim = "#aaaaaa"
+cfwd = "#CC3333"
+cback = "#1199DD"
+cbackdark = "#042367"
+caccent = "#E8BB22"
+cmid = "#6822AA"
+
+a.set_hl(0, "BrightFg", {fg=clight, bold=true})
+a.set_hl(0, "FwdFg", {fg=cfwd, bold=true})
+a.set_hl(0, "BackFg", {fg=cback, bold=true})
+a.set_hl(0, "BackMidFg", {fg="#0020CC"})
+a.set_hl(0, "BackMidFgDim", {fg="#111111", bg="#aaaaaa"})
+a.set_hl(0, "BackDarkFg", {fg=cbackdark})
+a.set_hl(0, "DimFg", {fg="#777777"})
+a.set_hl(0, "AccentFg", {fg=caccent, bold=true})
+
+ns = a.create_namespace'pres'
+
+function hl(nam, l, c, c2)
+  a.buf_add_highlight(0, ns, nam, l, c, c2)
+end
+
+function issue(r, num, name, date, idim)
+  date = date and (" ("..date..")") or ""
+  sf {r=r, c=8, text=num..": "..name..date, fn=function()
+    hl("BrightFg", 0, 0, #num+1)
+    hl("DimFg", 0, #num+2+#name, -1)
+    if idim then
+      hl("DimFg", 0, #num+1, #num+2+#name)
+    end
+  end}
+end
+
+function no_slide() end
+
+vim.cmd [[ hi Normal guibg=#080808 guifg=#e0e0e0]]
+
+vim.lsp.stop_client(vim.lsp.get_active_clients())
+vim.cmd [[set shortmess+=F]]
+vim.cmd [[set winblend=0]]
+s:slide('titlepage', function()
+  m.header 'Unicodes' -- TODO: fancy title
+
+  -- IMAGEN
+end)
+
+s:slide("intro", function()
+  m.header 'Overview'
+  sf {r=3, w=70, c=3, text=[[
+- baaaa
+  ]]}
+end)
+
+s:slide("whoami", function()
+  m.header 'Whoami'
+  sf {r=4, w=58, text=[[
+- One of the old---s at this point
+- core maintainer, focus on stuff
+- Paid contributor, very thanks to our sponsors
+  ]]}
+
+  -- IMAGEN
+
+  sf {r=12, text=[[
+- github.com/bfredl                matrix.to/#/@bfredl:matrix.org
+  ]], bg="#", fg=caccent}
+end)
+
+s:slide('unicode', function()
+  m.header 'The thing'
+end)
+
+s:slide('enda', function()
+  m.header 'Thanks for listening'
+end)
+
+s:show (s.slides[s.cur] and s.cur or "titlepage")
+
+vim.cmd [[map <pageDown> <cmd>lua s:mov(1)<cr>]]
+vim.cmd [[map <pageUp> <cmd>lua s:mov(-1)<cr>]]
+
