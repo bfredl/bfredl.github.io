d0652b26fd08ce98a840efcbdd6710efe38a43f9
bfredl <bjorn.linse@gmail.com>
2026-01-26 16:59:40 +0100

    mini.statusline

--- ri/lua/ri/init.lua
+++ ri/lua/ri/init.lua
@@ -1,11 +1,20 @@
+--  {{{ リ
 local first_run = not _G.ri
 local ri = _G.ri or {}
-_G.ri = ri
+_G.ri = ri -- }}}
 
 function ri.on_enter(startup)
   _G.ari = true
 end
 
+-- packages {{{
+vim.pack.add {
+  'https://github.com/nvim-mini/mini.nvim';
+}
+-- }}}
+-- mini {{{
+require'mini.statusline'.setup {}
+-- }}}
 -- epilogue {{{
 if first_run then
   vim.cmd [[autocmd VimEnter * lua _G.ri.on_enter(true)]]
