ed3dc24fea0eba31b24a924c0fc96a113e43eee2
bfredl <bjorn.linse@gmail.com>
2026-02-07 09:32:23 +0100

    feff

--- ri/lua/ri/rtp_checker.lua
+++ ri/lua/ri/rtp_checker.lua
@@ -3,15 +3,15 @@ function M.check_it()
   local rtp = vim.o.rtp
   local cached = vim.api.nvim__runtime_inspect()
   for _,item in ipairs(cached) do
-    print(vim.inspect(item))
-    local pos = item[5]+1
+    print(item.path, item.pos_in_rtp, item.pack_inserted and "pack" or "-", item.after and "after" or "|")
+    local pos = item.pos_in_rtp+1
     -- print ("checka:", string.sub(rtp, pos-1, pos-1))
     if pos > 1 and string.sub(rtp, pos-1, pos-1) ~= "," then
       print "WEIRD FAIL"
     end
     local next_pos = string.find(rtp, ",", pos, true)
     local sub = string.sub(rtp, pos, next_pos and (next_pos-1))
-    if (sub == item[1]) then
+    if (sub == item.path) then
       print("AXACT")
     else
       print ("is from:", sub)
